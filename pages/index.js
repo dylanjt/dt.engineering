254183c07bb7d7d818115f2ed39979dd545ea074
Dylan Taylor <dylan@Dylans-MacBook-Pro.local>
2022-01-20 21:21:34 -0500

    update lang again

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,12 @@ const Index = ({ form }) => {
         <div className="font-semibold text-base">Hi there, my name is Dylan Taylor.</div>
         <div>I'm a fullstack engineer in Washington, DC.</div>
         <div>
-          I help teams build performant cloud applications that are approachable to newly hired
-          engineers, maintainable down the road, and flexible to evolving needs. For most SaaS
-          enterprises, the architecture and quality of their code serves as the foundation of
-          company growth for years to come. Engineering decisions matter.
+          I help SaaS teams build foundational software systems that are intuitive, maintainable,
+          and flexible to evolving needs. With 7+ years of industry experience, these are some of
+          the technologies I most often use:
         </div>
-        <div>
-          With 7+ years of industry experience, these are some of the technologies I most often use:
-        </div>
-        <div className="flex flex-col md:flex-row gap-4 font-mono text-purple-300 text-sm">
-          <div>
+        <div className="flex flex-row gap-4 font-mono text-purple-300 text-sm overflow-x-auto">
+          <div className="shrink-0">
             <span className="font-semibold underline">Frontend</span>
             <ul>
               <li>React</li>
@@ -25,7 +21,7 @@ const Index = ({ form }) => {
               <li>Tailwind CSS</li>
             </ul>
           </div>
-          <div>
+          <div className="shrink-0">
             <span className="font-semibold underline">Backend</span>
             <ul>
               <li>Node.js</li>
@@ -33,7 +29,7 @@ const Index = ({ form }) => {
               <li>Go</li>
             </ul>
           </div>
-          <div>
+          <div className="shrink-0">
             <span className="font-semibold underline">Data Store</span>
             <ul>
               <li>PostgreSQL</li>
@@ -41,7 +37,7 @@ const Index = ({ form }) => {
               <li>Redis</li>
             </ul>
           </div>
-          <div>
+          <div className="shrink-0">
             <span className="font-semibold underline">Cloud</span>
             <ul>
               <li>AWS (Fargate, Lambda, S3, SQS, & more)</li>
@@ -49,13 +45,6 @@ const Index = ({ form }) => {
               <li>GitHub Actions</li>
             </ul>
           </div>
-        </div>
-        <div>
-          By leveraging battle-tested and industry-standard technologies like the ones above, teams
-          can tap into a vibrant ecosystem of open source tooling, and unlock a wide pool of talent
-          for future hiring. Combined with thoughtful development patterns and forward-leaning
-          structural decisions, they can also ensure a solid proprietary core that will continuously
-          meet business goals in tandem with company growth.
         </div>
         <div>
           <span className="font-semibold">Are you looking for engineering leadership?</span> That's
