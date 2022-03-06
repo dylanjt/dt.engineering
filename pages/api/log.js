52298274eb0314732b6a18f1d658bf4a204dbf3f
Dylan Taylor <dylan@dt.engineering>
2022-03-06 22:17:40 +0000

    log enhancements

--- pages/api/log.js
+++ pages/api/log.js
@@ -5,10 +5,9 @@ const parser = new Parser()
 const logAPI = async (req, res) => {
   if (req.method === 'GET') {
     const feed = await parser.parseURL('https://bg.raindrop.io/rss/public/23456285')
-    res.json(feed.items)
+    res.json(feed?.items)
     return
   }
-
   res.status(404).send()
 }
 
--- pages/log.js
+++ pages/log.js
@@ -7,13 +7,16 @@ dayjs.extend(LocalizedFormat)
 
 const Log = () => {
   const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const getItems = async () => {
-      const res = await fetch('/api/log').then((x) => x.json())
+      setLoading(true)
+      const res = await fetch('/api/log').then((x) => x?.json())
       if (res) {
         setItems(res)
       }
+      setLoading(false)
     }
     getItems()
   }, [])
@@ -25,23 +28,40 @@ const Log = () => {
       </Head>
       <div className="space-y-4">
         <div className="font-semibold text-base">Log</div>
-        <div className="pt-4 pb-8 space-y-8">
-          {items.map((item, i) => (
-            <div key={i} className="group">
-              <a href={item.link}>
-                <div className="text-xl decoration-1 underline-offset-2 group-hover:underline">
-                  {item.title}
-                </div>
-                <div className="space-x-1 text-xs text-gray-500">
-                  <span>{dayjs(item.isoDate).format('L')}</span>
-                  <span className="font-serif font-thinnest italic">from</span>
-                  <span>{parseDomain(item.link)}</span>
-                </div>
-                <div className="text-gray-400">{item.contentSnippet}</div>
-              </a>
-            </div>
-          ))}
+        <div className="space-x-1">
+          <span>
+            A collection of articles, mostly centered around technology or politics, that caught my
+            eye.
+          </span>
+          <a href="https://raindrop.io/dylant/muse-23456285" className="underline">
+            Explore on Raindrop.io
+          </a>
+          <span>or</span>
+          <a href="https://bg.raindrop.io/rss/public/23456285" className="underline">
+            subscribe via RSS
+          </a>
+          .
         </div>
+        {loading && <div className="pt-4">Loading...</div>}
+        {!loading && (
+          <div className="pt-4 pb-8 space-y-8">
+            {items.map((item, i) => (
+              <div key={i} className="group">
+                <a href={item.link}>
+                  <div className="text-xl decoration-1 underline-offset-2 group-hover:underline">
+                    {item.title}
+                  </div>
+                  <div className="space-x-1 text-xs text-gray-500">
+                    <span>{dayjs(item.isoDate).format('L')}</span>
+                    <span className="font-serif font-thinnest italic">from</span>
+                    <span>{parseDomain(item.link)}</span>
+                  </div>
+                  <div className="text-gray-400">{item.contentSnippet}</div>
+                </a>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   )
